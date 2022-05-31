484e6b4b533c3f51e4ceb992d81c30013f3e25b7
sigridvg <sigridvg96@gmail.com>
2022-05-31 15:21:40 -0300

    collections loops

--- src/PracticeCollections/CollectionDemo.java
+++ src/PracticeCollections/CollectionDemo.java
@@ -4,17 +4,17 @@ import java.util.*;
 
 public class CollectionDemo {
     public static void main(String arg[]){
-        //setDemo();
+       // setDemo();
         //listDemo();
         //queueDemo();
-        //mapDemo();
+        mapDemo();
     }
 
 
     public static void setDemo(){
-        Set fruit = new HashSet();
+        Set<String> fruit = new HashSet();
         fruit.add("apple");
-        fruit.add("lemon")
+        fruit.add("lemon");
         fruit.add("banana");
         fruit.add("orange");
         fruit.add("lemon");
@@ -22,7 +22,18 @@ public class CollectionDemo {
         System.out.println(fruit.size());
         System.out.println(fruit);
 
+        var i = fruit.iterator();
+        while (i.hasNext()){
+            System.out.println(i.next());
+        }
+
+        for( String item: fruit){
+            System.out.println(item);
+        }
+        fruit.forEach(j -> System.out.println(j));
+        fruit.forEach(System.out::println);
     }
+
     public static void listDemo(){
         List fruit = new ArrayList();
         fruit.add("apple");
@@ -35,6 +46,7 @@ public class CollectionDemo {
         System.out.println(fruit);
 
     }
+
     public static void queueDemo(){
         Queue fruit = new LinkedList();
         fruit.add("apple");
@@ -46,8 +58,9 @@ public class CollectionDemo {
         System.out.println(fruit.size());
         System.out.println(fruit);
     }
+
     public static void mapDemo(){
-        Map fruitCalories = new HashMap();
+        Map<String, Integer> fruitCalories = new HashMap();//have to defined a data type wich it can iterate a map
         fruitCalories.put("apple", 95);
         fruitCalories.put("banana", 55);
         fruitCalories.put("lemon", 90);
@@ -56,6 +69,19 @@ public class CollectionDemo {
 
         System.out.println(fruitCalories.size());
         System.out.println(fruitCalories);
+
+        System.out.println(fruitCalories.get("lemon"));
+
+        System.out.println(fruitCalories.entrySet());
+
+        fruitCalories.remove("orange");
+        System.out.println(fruitCalories);
+
+        for(var entry: fruitCalories.entrySet()){
+            System.out.println(entry.getValue());
+        }
+
+        fruitCalories.forEach((k,v)-> System.out.println("Fruit: "+ k + ", calories: "+ v));
     }
 
 }
